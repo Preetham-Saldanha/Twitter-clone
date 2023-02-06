47d974128dba3872413d8bf00b21c95e1ce47378
CL-Preetham-Saldanha <preetham@cirruslabs.io>
2023-02-07 00:57:07 +0530

    added profile page

---
+++ frontend/components/ProfileSection.tsx
@@ -0,0 +1,60 @@
+import { ArrowLeftIcon, BackwardIcon } from '@heroicons/react/24/outline'
+import Image from 'next/image'
+import React, { useState } from 'react'
+import { UserAuth } from '../typings'
+import twitterLogo from "../public/Twitter_bird_logo.png"
+import useAuth from '../hooks/useAuth'
+
+function ProfileSection(props: UserAuth) {
+
+  const { auth }: any = useAuth();
+  const [selectedBar, setSelectedBar]= useState<number>(1)
+const handleNavbarClick =(section : number)=>{
+setSelectedBar(section)
+}
+
+  return (
+    <div className='col-span-7 lg:col-span-5 border-gray-100 border-x max-h-screen overflow-scroll scrollbar-hide h-screen'>
+
+      <div className='flex mt-3' >
+
+        <div className='px-3'><ArrowLeftIcon className='h-6 w-6' /></div>
+        <div>
+          {/* <p>{props.firstname} {props.lastname}</p> */}
+          <p className='font-bold'>Preetham Saldanha</p>
+          <p className='text-sm'>5 tweets</p>
+        </div>
+      </div>
+
+      <div className='h-2/6 w-full '>
+        <div className='bg-slate-300 h-4/6 w-full'>
+        </div>
+        <div className=' flex relative bg-black h-32 w-32 ml-4 rounded-full -mt-16 justify-center items-center'>
+          <Image src={twitterLogo} alt="" height={70} width={70} />
+        </div>
+
+        <div className='flex w-full justify-end pr-5 '>
+          {props.username === auth.username ? <button className=' relative -mt-12 h-10 w-1/6 border-gray-500 font-semibold border rounded-3xl hover:bg-slate-500'>Edit Profile</button> : <button className=' relative -mt-12 h-10 w-1/6 bg-black font-semibold border rounded-3xl text-white'>Follow</button>}
+        </div>
+      </div>
+
+      <div className=' relative flex-col px-3 -mt-3'>
+        <p className='font-bold text-2xl leading-5'>Preetham Saldanha</p>
+        <p className=' text-gray-500 '> @preetham</p>
+        <p className=' text-gray-500 mt-3'> joined january 2022</p>
+        <p className=' text-gray-500 mt-3'> <span className='text-black font-semibold'>1</span> follower <span className='text-black font-semibold'>1</span> followed</p>
+      </div>
+
+      {/* navbar for profile */}
+
+      <div className='flex w-full text-center mt-4 h-12 cursor-pointer '>
+        <div className='text-gray-500 hover:bg-slate-200 w-1/4 pt-2 ' onClick={()=>handleNavbarClick(1)}>Tweets {selectedBar==1 &&<p className='border-b-4 border-twitter relative top-3 rounded-xl'></p>}</div>
+        <div  className='text-gray-500 hover:bg-slate-200 w-4/12 pt-2' onClick={()=>handleNavbarClick(2)}>Tweets & Replies {selectedBar==2 && <p className='border-b-4 border-twitter relative top-3 rounded-xl'></p>}</div>
+        <div className='text-gray-500 hover:bg-slate-200 w-1/4 pt-2' onClick={()=>handleNavbarClick(3)}> Media {selectedBar==3 && <p className='border-b-4 border-twitter relative top-3 rounded-xl'></p>}</div>
+        <div className='text-gray-500 hover:bg-slate-200 w-1/4 pt-2' onClick={()=>handleNavbarClick(4)}>Likes {selectedBar==4 && <p className='border-b-4 border-twitter relative top-3 rounded-xl'></p>}</div>
+      </div>
+    </div>
+  )
+}
+
+export default ProfileSection
---
+++ frontend/pages/profile.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import ProfileSection from '../components/ProfileSection'
+import SideBar from '../components/SideBar'
+import Widget from '../components/Widget'
+
+function Profile() {
+  return (
+    <div className='grid grid-cols-9  font-roboto  lg:max-w-6xl mx-auto'>        
+    <SideBar row={5} />
+
+  <ProfileSection/>
+
+    <Widget /></div>
+)
+  
+}
+
+export default Profile
