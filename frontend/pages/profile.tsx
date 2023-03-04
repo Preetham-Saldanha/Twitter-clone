42f9a08fff5fe632b83ed76254accb6c74ebc63e
Preetham-Saldanha <preethamsaldanha26@gmail.com>
2023-03-04 11:35:21 +0530

    implemented layout for increasing reusability of sidebar

--- frontend/pages/profile.tsx
+++ frontend/pages/profile.tsx
@@ -1,6 +1,7 @@
 import { useRouter } from 'next/router'
 import React, { useEffect, useState } from 'react'
 import { axiosPrivate } from '../api_utils/axios'
+import Layout from '../components/Layout'
 import ProfileSection from '../components/ProfileSection'
 import SideBar from '../components/SideBar'
 import Widget from '../components/Widget'
@@ -10,30 +11,16 @@ import { profileDataType } from '../typings'
 function Profile() {
 
   const { auth }: any = useAuth()
-  const [profileData, setProfileData] = useState<profileDataType>()
   const router = useRouter()
 
   const name: string | string[] = router.query?.username ? router.query.username : auth.username
-  // const [user, setUser] = useState<string | string[]>(name)
-  // async function getUserDetails() {
-  //   const result : profileDataType= await axiosPrivate.get(`/api/v1/user/${auth.username}`)
-  //   if (result) {
-  //     console.log(result)
-  //     setProfileData(result)
-  //   }
-  // }
-  // useEffect(() => {
-  //   getUserDetails()
-  // }, [])
-
 
   return (
-    <div className='grid grid-cols-9  font-roboto  lg:max-w-6xl mx-auto'>
-      <SideBar row={auth?.username === name ? 5 : -1} />
+    <Layout row={auth?.username === name ? 5 : -1}>
 
       <ProfileSection username={name} />
+    </Layout>
 
-      <Widget /></div>
   )
 
 }
